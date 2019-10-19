617163818599763e16c77b4d7dc891037d2f707b
Moritz <email@moritz-huebner.de>
2019-10-19 11:14:22 +1100

    Improved data cleaning

--- cleaning.py
+++ cleaning.py
@@ -17,7 +17,6 @@ stop_words = set(stopwords.words('english'))
 
 def get_training_and_testing_data(csv_file):
     data = pd.read_csv(csv_file)
-    # data = data.iloc[:100, :]
     lemmatizer = WordNetLemmatizer()
 
     data['text'] = [data['text'][i].translate(str.maketrans('', '', string.punctuation)) for i in range(len(data))]
@@ -31,7 +30,3 @@ def get_training_and_testing_data(csv_file):
     train = data.iloc[:5500, :]
     test = data.iloc[5500:, :]
     return train, test
-
-
-train_data, test_data = get_training_and_testing_data('train_data.csv')
-print(train_data)
